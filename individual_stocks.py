e95478c0e18f131453560c0989835d6927ffb9a4
Anup Timsina <code.anup23@gmail.com>
2025-02-15 20:52:33 +0545

    Inside each script sucessfully

--- individual_stocks.py
+++ individual_stocks.py
@@ -1,7 +1,7 @@
 from fetch_data import get_stock_data
 import pandas as pd
 import requests as rq
-
+from bs4 import BeautifulSoup as bs
 # loading data in df
 stocks_df = get_stock_data()
 
@@ -12,4 +12,7 @@ stock_list = stocks_df['symbol'].to_list()
 individual_stocks_url = "https://sharehubnepal.com/company/"
 
 response = rq.get(individual_stocks_url+stock_list[0])
-print(response.status_code)
+if response.status_code == 200:
+    print("inside each script sucessfully")
+else:
+    print(f"Unable to fetch data wuth response.code = {response.code}")
