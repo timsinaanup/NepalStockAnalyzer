5d5572362afe645e22a5494a11e696f2995c96d0
Anup Timsina <code.anup23@gmail.com>
2025-02-16 06:39:23 +0545

    added config for selenium

--- config.py
+++ config.py
@@ -3,3 +3,10 @@ from bs4 import BeautifulSoup as bs
 import json
 import numpy as np
 import pandas as pd
+import time
+
+#for dynamic js pages (fundamental signals, )
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
