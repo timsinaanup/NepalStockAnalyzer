95d1735e3e9a036dee073e80dc256dc9d8627115
Anup Timsina <code.anup23@gmail.com>
2025-02-17 21:49:10 +0545

    replaced selenium by network req api for table data

--- fundamentals.py
+++ fundamentals.py
@@ -1,33 +1,12 @@
+from scraper import get_json
 from fetch_data import get_stock_namelist
-from config import *
-from scraper import get_soup
+from config import pd,bs
 
-# Set up Chrome options
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')  # Optional: Runs browser in background without opening a window
-options.add_argument('--disable-gpu')
-
-# Initialize WebDriver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-stock_name = get_stock_namelist()[0]
-url = f"https://sharehubnepal.com/company/{stock_name}/fundamental-analysis/reports"
-
-driver.get(url)
-
-# Optional: Wait for some content to load dynamically (e.g., waiting for a specific element)
-time.sleep(5)  # Give some time for content to load (you can adjust the wait time as needed)
-
-# Now you can interact with the page using BeautifulSoup or by using Selenium directly
-page_source = driver.page_source  # Get the entire HTML content after JavaScript loads
-
-soup = bs(page_source, 'html.parser')
-
-
-table = soup.find('table', class_="min-w-max w-full caption-bottom border-collapse") 
-thead = table.find('thead', class_= "dark:bg-dark-container bg-white dark:text-text-light border-b h-8 text-sm font-bold uppercase")
-tbody = table.find('tbody', class_="dark:text-text-light text-black font-bold text-xs")
-th_tr = thead.find('tr', class_ = 'hover:bg-muted/50 border-b dark:border-gray-700 border-gray-200')
-
-headers = [th.text.strip() for th in th_tr.find_all('th')]  # Extract column headers
-
+def script_fundamentals():
+    script_name = get_stock_namelist()[52]
+    url = f'https://sharehubnepal.com/data/api/v1/fundamental/values/{script_name}'
+    fundamentals_json = get_json(url)
+    return fundamentals_json.get('data')[0]
+    
+    
+print(script_fundamentals())
